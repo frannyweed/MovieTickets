f4b229873e72bc26a8a4b1788a9797df7615f741
JJ <jbernhardt@fwparker.org>
2017-05-03 09:59:50 -0500

    more work

--- purchase.py
+++ purchase.py
@@ -10,17 +10,33 @@ Provides the Records class.
 
 """
 
+
 class Records():
     """Records class in separate file from __main__"""
+    record = 'MovieTickets.rec'
+    ledger = 'MovieTickets.leg'
+    from csv import DictReader as __reader
+    from csv import DictWriter as __writer
+
     def __init__(self):
         pass
 
-    def write_to_ledger(self,date,movie,time,tickets):
-        """a ledger to permanently record each purchase (date,movie,time,tickets)"""
-        pass
+    def claim_tickets(self, date, movie, time, number):
+        """function(s) and/or method(s) to "claim" tickets
+        :param date
+        :param movie
+        :param time
+        :param number
+        """
 
-    def write_to_record(self):
-        """a record to permanently record total purchase for each day and showing"""
+    def __write(self, filename, data):
+        """private method to write some data. data is a list of dict."""
+        with open(filename, 'w') as f:
+            writer = self.__writer(f, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
 
-    def claim_tickets(self):
-        """function(s) and/or method(s) to "claim" tickets"""
+    def __read(self, filename):
+        """private method to read some data"""
+        with open(filename) as f:
+            return [dict(row) for row in self.__reader(f)]
