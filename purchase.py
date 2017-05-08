fc0623c0c2dbdbb5ed25706f7c88274f45295d8a
JJ <jbernhardt@fwparker.org>
2017-05-08 13:48:07 -0500

    more more work

--- purchase.py
+++ purchase.py
@@ -2,39 +2,50 @@
 purchase.py
 Provides the Records class.
 
-- 
-- both record and ledger kept as csv files 
-- checks to ensure no more than 10 tickets per showing
-- checks to ensure no more than 20 tickets per day
-- ledger and record are updated after each purpose
-
+@author: JJ
 """
 
 
-class Records():
+class check_ticket():
     """Records class in separate file from __main__"""
     record = 'MovieTickets.rec'
     ledger = 'MovieTickets.leg'
+
+    MovieError = "Already bought 10 of those..."
+    DateError = "20 tickets in one day??"
+
     from csv import DictReader as __reader
     from csv import DictWriter as __writer
-
     def __init__(self):
         pass
 
-    def claim_tickets(self, date, movie, time, number):
-        """function(s) and/or method(s) to "claim" tickets
-        :param date
-        :param movie
-        :param time
-        :param number
+    def __call__(self, date, movie, time, number):
+        """ function(s) and/or method(s) to "claim" tickets
+            :param date
+            :param movie
+            :param time
+            :param number
         """
+        movie_count = 0
+        day_count = 0
+        for purchase in self.__read(self.ledger):
+            movie_count += 1 if purchase[movie] == movie else 0
+            day_count += 1 if purchase[date] == date else 0
+        if movie_count > 10:
+            return self.MovieError
+        if day_count > 20:
+            return self.DateError
+        # checks to ensure no more than 10 tickets per showing
+        # checks to ensure no more than 20 tickets per day
+        # ledger and record are updated after each purpose
+        self.__write({"date": date, "movie": movie, "time": time, "number": number})
 
     def __write(self, filename, data):
-        """private method to write some data. data is a list of dict."""
+        """private method to append some data. data is a list of dict."""
         with open(filename, 'w') as f:
             writer = self.__writer(f, fieldnames=data[0].keys())
             writer.writeheader()
-            writer.writerows(data)
+            writer.writerows(self.__read(filename).append(data))
 
     def __read(self, filename):
         """private method to read some data"""
