6370fd5bb546d88f167fac15bd00553bc81814d0
ninasachs <nsachs@fwparker.org>
2017-05-10 09:38:55 -0500

    hi

--- beautiful_soups.py
+++ beautiful_soups.py
@@ -3,18 +3,16 @@ import urllib.request
 
 
 
-url = "http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage"
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page.read(), "html.parser")
-soup.prettify()
+
 
 #print(soup)
 #based on the day, list movies, times, and prices
 
-def Soup_time():
-
-    #TITLES
-    movie_titles_list = [x.text.strip() for x in soup.findAll("a", {"class": "dark showtimes-movie-title"})]
+def get_dates():
+    url = "http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), "html.parser")
+    soup.prettify()
 
     #DATES
     dates = [x.text.strip() for x in soup.findAll("a", {"class": "date-area"})]
@@ -25,17 +23,30 @@ def Soup_time():
     #URLS
     urls = [x.get('href') for x in soup.findAll("a", {"class": "date-area"})]
     urls = urls[:7]
+    for i in urls:
+        print(i)
 
-    #TIMES
-    times = [x.text.strip() for x in soup.findAll("time", {"class": "timeInfo"})]
+    #print(dates)
+    #print(urls)
 
+def get_movies(url):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), "html.parser")
+    soup.prettify()
+    movie_titles_list = [x.text.strip() for x in soup.findAll("a", {"class": "dark showtimes-movie-title"})]
     print(movie_titles_list)
+
+def get_times(url):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), "html.parser")
+    soup.prettify()
+    times = [x.text.strip() for x in soup.findAll("time", {"class": "timeInfo"})]
     print(times)
-    print(dates)
-    print(urls)
-
-
 
 if __name__ == "__main__":
-    Soup_time()
+    get_dates()
+    get_movies("http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage?date=5/10/2017")
+    get_times("http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage?date=5/10/2017")
+
+
 
